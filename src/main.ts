f22b670913131ba179df473660146f4ca97aaf58
Daniel Loriaux <daniel.loriaux@brightfox.eu>
2024-06-03 21:19:13 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+export function main(inA: string, inB: string) {
+    console.log('inA', inA);
+    console.log('inB', inB);
+    const out = inA + inB;
+    console.log('out', out);
+    return out;
+}
