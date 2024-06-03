ed27a43e3dd46fddd6ccd7a4ca267d93d5806db6
Daniel Loriaux <daniel.loriaux@brightfox.eu>
2024-06-03 21:58:45 +0200

    path fix

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
-export function main(inA: string, inB: string) {
-    console.log('inA', inA);
-    console.log('inB', inB);
-    const out = inA + inB;
-    console.log('out', out);
-    return out;
+type Input = {
+    inA: string;
+    inB: string;
+}
+
+type Output = {
+    out: string
+}
+
+export function main(input: Input): Output {
+    console.log('inA', input.inA);
+    console.log('inB', input.inB);
+    const result = {
+        out: input.inA + input.inB
+    }
+    console.log('out', result.out);
+    return result;
 }
